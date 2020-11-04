92d21719d5fad10de8fddf85cc427758e2a4ca3f
Artsiom Trubchyk <aglitchman@gmail.com>
2020-11-04 14:14:01 +0300

    Removed demo game

--- example/app/consts.lua
+++ example/app/consts.lua
@@ -3,6 +3,6 @@ local M = {}
 M.game_width = 1000
 M.game_height = 1500
 
-M.gui_order = {BACKGROUND = 0, DEFAULT = 1, QUICKINV = 5, PAUSE = 10, PHOTOMODE = 10, TRANSITION = 14, DEBUG = 15}
+M.gui_order = {BACKGROUND = 0, DEFAULT = 1, TRANSITION = 14, DEBUG = 15}
 
 return M
